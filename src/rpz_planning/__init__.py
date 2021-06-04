ef4d6d6242f71a82519e4b6614df504f352ae6f7
RuslanAgishev <agishrus@fel.cvut.cz>
2021-06-04 18:35:20 +0200

    minor updates: for the last commit to work not only with python3

--- src/rpz_planning/__init__.py
+++ src/rpz_planning/__init__.py
@@ -1,3 +1,6 @@
 from __future__ import absolute_import, division, print_function
 from .visibility import point_visibility
-from .pcl_mesh_metrics import *
+try:
+    from .pcl_mesh_metrics import *
+except:
+    print('Pytorch3d is propbably not installed')
